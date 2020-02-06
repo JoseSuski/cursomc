b1455135d4b97a939287844ca1738d5d59606e60
Jose Suski <flasuski@gmail.com>
2020-02-06 16:46:09 -0300

    validacao sintatica com Bean Validation

--- src/main/java/com/josesuski/cursomc/services/CategoriaService.java
+++ src/main/java/com/josesuski/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.josesuski.cursomc.domain.Categoria;
+import com.josesuski.cursomc.dto.CategoriaDTO;
 import com.josesuski.cursomc.repositories.CategoriaRepository;
 import com.josesuski.cursomc.services.exceptions.DataIntegrityException;
 import com.josesuski.cursomc.services.exceptions.ObjectNoFoundException;
@@ -60,12 +61,16 @@ public class CategoriaService {
 		return repo.findAll();
 	}
 
-	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
-		
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
-		
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
 		return repo.findAll(pageRequest);
-		
+
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
