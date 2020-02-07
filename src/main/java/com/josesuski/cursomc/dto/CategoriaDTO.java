587783ca5e4191111ea38d60733b061b4ae4db6b
Jose Suski <flasuski@gmail.com>
2020-02-07 13:46:03 -0300

    PUT GET DELETE para Cliente

--- src/main/java/com/josesuski/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/josesuski/cursomc/dto/CategoriaDTO.java
@@ -9,38 +9,38 @@ import javax.validation.constraints.NotEmpty;
 
 public class CategoriaDTO implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private Integer id;
-		
-	@NotEmpty(message="Preenchimento obrigatório")
-	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
-	private String nome;
+    private Integer id;
 
-	public CategoriaDTO(Categoria obj) {
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
+    private String nome;
 
-		id = obj.getId();
-		nome = obj.getNome();
+    public CategoriaDTO(Categoria obj) {
 
-	}
+        id = obj.getId();
+        nome = obj.getNome();
 
-	public CategoriaDTO() {
-	}
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public CategoriaDTO() {
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
 }
