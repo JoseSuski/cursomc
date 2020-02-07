158f9b15d739d8cd77fe71e6f14f00249c73081c
Jose Suski <flasuski@gmail.com>
2020-02-07 17:25:39 -0300

    Validação customizada email nao repetido na att de Cliente

--- src/main/java/com/josesuski/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/josesuski/cursomc/dto/ClienteDTO.java
@@ -1,13 +1,14 @@
 package com.josesuski.cursomc.dto;
 
-import ch.qos.logback.core.net.server.Client;
 import com.josesuski.cursomc.domain.Cliente;
+import com.josesuski.cursomc.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
