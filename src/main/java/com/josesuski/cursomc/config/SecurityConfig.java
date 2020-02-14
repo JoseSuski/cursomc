b8c9448600812fe173687e677c2aac94051a8d44
Jose Suski <flasuski@gmail.com>
2020-02-14 15:55:10 -0300

    Implementando autorização

--- src/main/java/com/josesuski/cursomc/config/SecurityConfig.java
+++ src/main/java/com/josesuski/cursomc/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.josesuski.cursomc.config;
 
 import com.josesuski.cursomc.security.JWTAuthenticationFilter;
+import com.josesuski.cursomc.security.JWTAuthorizationFilter;
 import com.josesuski.cursomc.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -53,7 +54,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
-        http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
